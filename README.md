6314c0dfeabbf91c40547df1886cd2c223a04f40
devkilyungi <victorkilyungi@gmail.com>
2023-06-27 15:19:09 +0300

    final commit: app built

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # grid-courses
 The app built in this exercise is a modified version of the Owl app courses screen. 
+
+All photos in this repo are for the purposes of building the Courses app for the Android Basics with Compose course. 
+All photos by Romain Guy. 
+All photos are licensed under CC0 1.0 Universal: https://creativecommons.org/publicdomain/zero/1.0/
--- app/src/main/java/com/example/grid/MainActivity.kt
+++ app/src/main/java/com/example/grid/MainActivity.kt
@@ -3,13 +3,37 @@ package com.example.grid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.grid.data.DataSource
+import com.example.grid.model.Topic
 import com.example.grid.ui.theme.GridTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +46,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GridCoursesApp()
+                    GridCoursesApp(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)))
                 }
             }
         }
@@ -30,12 +54,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GridCoursesApp() {
+fun GridCoursesApp(modifier: Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        userScrollEnabled = true,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
+}
 
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.image),
+                    contentDescription = null,
+                    modifier
+                        .size(height = 68.dp, width = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Column(
+                modifier = modifier.padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp,
+                    bottom = 0.dp
+                )
+            ) {
+                Text(
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = modifier.height(8.dp))
+                Row {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = modifier.width(8.dp))
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        modifier = modifier.align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GridCoursesApp()
+fun TopicPreview() {
+    val topic = Topic(R.string.music, 212, R.drawable.music)
+
+    TopicCard(topic = topic)
 }
